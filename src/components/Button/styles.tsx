6e1883269d02a594d568156f4337a855b3560630
biagavirete <biagavirete@gmail.com>
2021-03-06 15:53:29 -0300

    fix: correct imports

--- src/components/Button/styles.tsx
+++ src/components/Button/styles.tsx
@@ -1,5 +1,5 @@
 import styled, { css } from 'styled-components';
-import { ButtonProps } from './Button';
+import { ButtonProps } from '../Button';
 
 export const ButtonStyled = styled.button<ButtonProps>`
   width: auto;
