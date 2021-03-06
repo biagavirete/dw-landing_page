93d0f1c59a8baa221c70daa82456179a94823ac8
biagavirete <biagavirete@gmail.com>
2021-03-06 19:30:12 -0300

    feat: add hr to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import NavBar from './components/Navbar';
 import Hero from './components/Hero';
 import { Container } from './styles';
 import PinkArea from './components/PinkArea';
+import Features from './components/Features';
+import Bottom from './components/Bottom';
 
 function App() {
   return (
@@ -11,6 +13,9 @@ function App() {
         <NavBar />
         <Hero />
         <PinkArea />
+        <Features />
+        <hr />
+        <Bottom />
       </Container>
     </>
   );
--- src/styles.ts
+++ src/styles.ts
@@ -1,9 +1,20 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 70rem;
+  width: 70rem;
   padding: 2.875rem 15rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
+
+  hr {
+    margin-top: 6.2rem;
+    width: 100vw;
+    margin-left: -50vw;
+    position: relative;
+    left: 50%;
+    height: 3px;
+    background-color: rgba(156,105,226, 0.2);
+    border: none;
+  }
 `;
