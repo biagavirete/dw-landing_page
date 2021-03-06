f8faf182806e184269a89e76434b0d644431b6e9
biagavirete <biagavirete@gmail.com>
2021-03-06 19:28:55 -0300

    fix: correct hero image positioning

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -10,9 +10,9 @@ const Hero = () => {
 
       <div className="text-container">
         <h1>Save your data storage here.</h1>
-        <p>Data Warehouse is a data storage area that has been
-        tested for security, so you can store your data here
-          safely but not be afraid of being stolen by others.</p>
+        <p>Data Warehouse is a data storage area that <br /> has been
+        tested for security, so you can store <br /> your data here
+          safely but not be afraid of <br /> being stolen by others.</p>
 
         <Button isShadowed={false} backgroundColor="#9C69E2">Learn more</Button>
 
--- src/components/Hero/styles.tsx
+++ src/components/Hero/styles.tsx
@@ -3,22 +3,32 @@ import styled from 'styled-components';
 export const Container = styled.div`
   margin-top: 6rem;
   display: flex;
+  flex-direction: row;
+  height: 42rem;
 
   .text-container {
-    width: 50rem;
+    width: 60%;
     display: flex;
     flex-direction: column;
+    z-index: 2;
+    position: relative;
 
     h1 {
       font-size: 5rem;
       font-weight: 700;
       color: #212353;
-      flex-wrap: wrap;
+      margin-bottom: 0;
     }
 
     p {
+      margin-top: 3rem;
       font-size: 1.125rem;
       color: #4B5D68;
+      line-height: 2rem;
+    }
+
+    button {
+      margin-top: 3rem;
     }
   }
 
@@ -26,6 +36,9 @@ export const Container = styled.div`
     margin-top: 15rem;
     width: 55rem;
     height: 29rem;
-
+    z-index: 1;
+    position: absolute;
+    top: 7.5rem;
+    right: 21.875rem;
   }
 `;
