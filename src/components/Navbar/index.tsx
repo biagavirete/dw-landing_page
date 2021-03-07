1e66a9fcb112a5c31359d4a161cf202f16dd8eb7
biagavirete <biagavirete@gmail.com>
2021-03-06 21:26:14 -0300

    feat: use logo svg as component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import { BsArrowRight } from 'react-icons/bs'
 import Button from '../Button';
+import LogoSvg from '../SVGs/Logo';
 import { Container } from './styles';
 
 const NavBar = () => {
 
   return (
     <Container>
-      <div className="logo">
-        <svg width="49" height="35" viewBox="0 0 49 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect y="15" width="20" height="20" rx="10" fill="#9C69E2" />
-          <rect x="29" width="20" height="35" rx="10" fill="#F063B8" />
-        </svg>
-      </div>
-
+      <LogoSvg />
       <nav>
         <ul>
           <li><a href="#">About</a></li>
