d88cec544dc93bcf50d11b964326ff201f5d6f34
biagavirete <biagavirete@gmail.com>
2021-03-07 12:36:12 -0300

    fix: correct hr height

--- src/styles.ts
+++ src/styles.ts
@@ -13,7 +13,7 @@ export const Container = styled.div`
     margin-left: -50vw;
     position: relative;
     left: 50%;
-    height: 3px;
+    height: 2px;
     background-color: rgba(156,105,226, 0.2);
     border: none;
   }
